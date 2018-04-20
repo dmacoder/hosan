3811f732b67dad5315a5b2cf2056ca18a14228d3
Se Chang Park <dmacoder@gmail.com>
2018-04-20 17:57:39 +0900

    Member(LoginDTO)

---
+++ src/main/java/com/cafe24/hosan/member/dto/LoginDTO.java
@@ -0,0 +1,31 @@
+package com.cafe24.hosan.member.dto;
+
+public class LoginDTO {
+	
+	private String user_id;
+	private String password;
+	private boolean useCookie;
+	
+	
+	public boolean isUseCookie() {
+		return useCookie;
+	}
+	public void setUseCookie(boolean useCookie) {
+		this.useCookie = useCookie;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	  
+}
